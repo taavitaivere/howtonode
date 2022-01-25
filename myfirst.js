ad95457e5bef6a380590926deceaa914be82a0fd
Taavi taivere <taavi.taivere@khk.ee>
2022-01-25 22:16:15 +0200

    Node.js read HTML

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var url = require('url');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
+    fs.readFile('demofile1.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
