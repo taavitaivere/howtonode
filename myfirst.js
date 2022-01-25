afbdecd285dafd6578331c3ef049be2baaead3d0
Taavi taivere <taavi.taivere@khk.ee>
2022-01-25 17:18:32 +0200

    first Modul

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
-
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time is currently: " + dt.myDateTime());
