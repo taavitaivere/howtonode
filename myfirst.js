7fa0c3add4638eba986370c74a14b45116282c16
Taavi taivere <taavi.taivere@khk.ee>
2022-01-25 22:40:57 +0200

    EventEmitter Object using emit()

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,13 @@
-var fs = require('fs');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-var rs = fs.createReadStream('./demofile.txt');
-rs.on('open', function () {
-    console.log('The file is open');
-});
+//Create an event handler:
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
